80b30f736491fa95dc118d5161a5254d98d76768
kiennt8 <kiennt8@rikkeisoft.com>
2026-02-09 11:11:58 +0700

    refactor: remove precision attribute from lat, lon columns

--- src/main/java/com/rk/WMS/order/model/Order.java
+++ src/main/java/com/rk/WMS/order/model/Order.java
@@ -28,64 +28,64 @@ public class Order {
     @Column(name = "order_code", nullable = false, unique = true, length = 50)
     private String code;
 
-    @Column(nullable = false)
-    private OrderStatus status;
+  @Column(nullable = false)
+  private OrderStatus status;
 
     @Column(name = "warehouse_id")
     private Long warehouseId;
 
-    //supplier
-    @Column(name = "supplier_name", nullable = false,length = 100)
-    private String supplierName;
+  //supplier
+  @Column(name = "supplier_name", nullable = false,length = 100)
+  private String supplierName;
 
-    @Column(name = "supplier_address", nullable = false,length = 200)
-    private String supplierAddress;
+  @Column(name = "supplier_address", nullable = false,length = 200)
+  private String supplierAddress;
 
-    @Column(name = "supplier_phone", nullable = false,length = 20)
-    private String supplierPhone;
+  @Column(name = "supplier_phone", nullable = false,length = 20)
+  private String supplierPhone;
 
-    @Column(name = "supplier_email", nullable = false,length = 100)
-    private String supplierEmail;
+  @Column(name = "supplier_email", nullable = false,length = 100)
+  private String supplierEmail;
 
-    //receiver
-    @Column(name = "receiver_name", nullable = false,length = 100)
-    private String receiverName;
+  //receiver
+  @Column(name = "receiver_name", nullable = false,length = 100)
+  private String receiverName;
 
-    @Column(name = "receiver_address", nullable = false,length = 200)
-    private String receiverAddress;
+  @Column(name = "receiver_address", nullable = false,length = 200)
+  private String receiverAddress;
 
-    @Column(name = "receiver_phone", nullable = false,length = 20)
-    private String receiverPhone;
+  @Column(name = "receiver_phone", nullable = false,length = 20)
+  private String receiverPhone;
 
-    @Column(name = "receiver_email", nullable = false,length = 100)
-    private String receiverEmail;
+  @Column(name = "receiver_email", nullable = false,length = 100)
+  private String receiverEmail;
 
     @Column(name = "receiver_lat", nullable = false)
     private BigDecimal receiverLat;
     @Column(name = "receiver_lon", nullable = false)
     private BigDecimal receiverLon;
 
-    //common
-    @CreationTimestamp
-    @Column(name = "created_at", nullable = false)
-    private LocalDateTime createdAt;
+  //common
+  @CreationTimestamp
+  @Column(name = "created_at", nullable = false)
+  private LocalDateTime createdAt;
 
-    @UpdateTimestamp
-    @Column(name = "updated_at", nullable = false)
-    private LocalDateTime updatedAt;
+  @UpdateTimestamp
+  @Column(name = "updated_at", nullable = false)
+  private LocalDateTime updatedAt;
 
     @Column(name = "stored_at")
     private LocalDateTime storedAt;
 
-    @Column(name = "dispatch_at")
-    private LocalDateTime dispatchAt;
+  @Column(name = "dispatch_at")
+  private LocalDateTime dispatchAt;
 
-    @Column(name = "delivery_at")
-    private LocalDateTime deliveryAt;
+  @Column(name = "delivery_at")
+  private LocalDateTime deliveryAt;
 
-    @Column(name = "returned_at")
-    private LocalDateTime returnedAt;
+  @Column(name = "returned_at")
+  private LocalDateTime returnedAt;
 
-    @Column(name = "failed_delivery_count")
-    private Integer failedDeliveryCount = 0;
+  @Column(name = "failed_delivery_count")
+  private Integer failedDeliveryCount = 0;
 }
