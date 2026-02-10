96851c9948e175276b31435ae4b84867924bd427
kiennt8 <kiennt8@rikkeisoft.com>
2026-02-10 11:10:27 +0700

    refactor: change context url for order and warehouse endpoints

--- src/main/java/com/rk/WMS/warehouse/controller/WarehouseController.java
+++ src/main/java/com/rk/WMS/warehouse/controller/WarehouseController.java
@@ -4,18 +4,20 @@ import com.rk.WMS.warehouse.dto.WarehouseBrief;
 import com.rk.WMS.warehouse.service.WarehouseService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j(topic = "WAREHOUSE-CONTROLLER")
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/v1/warehouses")
+@RequestMapping("/warehouses")
 public class WarehouseController {
 
   private final WarehouseService warehouseService;
-
   @GetMapping()
   public ResponseEntity<List<WarehouseBrief>> getAllWarehouses() {
     List<WarehouseBrief> warehouses = warehouseService.getAll();
